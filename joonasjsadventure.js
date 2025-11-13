d373ba0f5c98eded36daab99373d96e2180d6dfd
Joonas Lindberg <joonaslindberg@hotmail.com>
2025-11-13 23:11:40 +0200

    Created example gfx & code that puts example sprite on screen

--- joonasjsadventure.js
+++ joonasjsadventure.js
@@ -4,6 +4,10 @@ const screenHeight               = 909;
 const rowStride                  = screenWidth * 4;
 var canvas                       = document.getElementById("myCanvas");
 var ctx                          = canvas.getContext("2d");
+var playereBuffer                = document.getElementById("playereBuffer");
+var playereCtx                   = playereBuffer.getContext("2d");
+var playereSdata                 = playereCtx.createImageData(66, 157);
+var playereSprite                = document.getElementById("playere");
 
 let Application = PIXI.Application,
 	Container = PIXI.Container,
@@ -40,11 +44,25 @@ function gameLoop(delta)
 	state(delta);
 }
 
+function doSpriteTransparency(givenbufferctx, givenbuffer, givenpic, keyR, keyG, keyB)
+{
+	var sizeofit = 4 * givenbuffer.width * givenbuffer.height;
+	for(var tpPos = 0; tpPos < sizeofit; tpPos += 4)
+	{
+		if(givenpic.data[tpPos] == keyR && givenpic.data[tpPos+1] == keyG && givenpic.data[tpPos+2] == keyB) givenpic.data[tpPos+3] = 0;
+	}
+	givenbufferctx.putImageData(givenpic, 0, 0);
+}
+
 window.onload = function() {
 	imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	playereCtx.drawImage(playereSprite, 0, 0);
+	playereSdata = playereCtx.getImageData(0, 0, playereBuffer.width, playereBuffer.height);
+	doSpriteTransparency(playereCtx, playereBuffer, playereSdata, 52, 90, 72);
 };
 
 function play(delta)
 {
     if(imgData != null) ctx.putImageData(imgData, 0, 0);
+	ctx.putImageData(playereSdata, 60, 60);
 }
